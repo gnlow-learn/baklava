0cd81ac7fe9c5635db16783a93e72f02cf9a7573
Gnlow <gnlow@pm.me>
2025-02-05 17:09:11 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,12 +4,20 @@ import { createBaklava } from "https://esm.sh/baklavajs@2.6.0/dist/esm/bundle"
 import {
     defineNode,
     NumberInterface,
+    NodeInterface,
 } from "https://esm.sh/baklavajs@2.6.0"
 
 const baklava = createBaklava(document.body)
 baklava.editor.registerNodeType(defineNode({
-    type: "MyNode",
+    type: "Add",
     inputs: {
-        a: () => new NumberInterface("Number", 1),
+        a: () => new NumberInterface("a", 1),
+        b: () => new NumberInterface("b", 1),
+    },
+    outputs: {
+        c: () => new NodeInterface("c", 1),
+    },
+    calculate({ a, b }) {
+        return { c: a + b }
     }
 }))
